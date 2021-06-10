c4deba5db9663042cc03d9cc041e59844b3b6a96
Sérgio Guimarães de Andrade <sergio.guimaraes.ads@gmail.com>
2021-06-10 18:22:30 -0300

    Criar biometria

---
+++ src/main/java/br/com/zupacademy/proposta/model/Biometria.java
@@ -0,0 +1,44 @@
+package br.com.zupacademy.proposta.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Biometria {
+    @Id @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid2")
+	private String id;
+	@NotNull
+	@Lob
+	private byte[] fingerprint;
+	@CreationTimestamp
+	private LocalDateTime horarioCriacao;
+	
+	@Deprecated
+	public Biometria() { }
+	
+	public Biometria(@NotNull byte[] fingerprint) {
+		this.fingerprint = fingerprint;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public byte[] getFingerprint() {
+		return fingerprint;
+	}
+
+	public LocalDateTime getHorarioCriacao() {
+		return horarioCriacao;
+	}
+
+}
